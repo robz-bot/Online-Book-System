87f2d3455c4d6ed358b8433cec068e90c23b7567
robinrajesh <robinsoncruse9@gmail.com>
2021-01-16 20:10:51 +0530

    Day 2 Changes

--- Book_System/Book_System/Scripts/Book/jsBook.js
+++ Book_System/Book_System/Scripts/Book/jsBook.js
@@ -2,6 +2,40 @@
     this.Init = function () {
         $myApp.controller("BookController", function ($scope, customService) {
             $scope.value = "";
+            $scope.SearchBook = null;
+
+            $scope.getBookRateListBySearch = function (SearchBook) {
+                request = {}
+                request.search = ($scope.SearchBook != null && $scope.SearchBook != '' &&
+                    $scope.SearchBook != undefined) ? $scope.SearchBook : null;
+                customService.postData('Book/getBookRateListBySearch', request, function (data) {
+                    $scope.count = data.Item.length;
+                    if (data.isSuccess) {
+                        $scope.getBookRateList = data.Item;
+                    }
+                    else {
+                        alert(data.Message);
+                        location.reload();
+                    }
+                });
+            };
+
+            $scope.getBookRateList = function () {
+                request = {}
+                request.search = ($scope.SearchBook != null && $scope.SearchBook != '' &&
+                    $scope.SearchBook != undefined) ? $scope.SearchBook : null;
+                customService.postData('Book/getBookRateList', search, function (data) {
+                    $scope.count = data.Item.length;
+                    if (data.isSuccess) {
+                        $scope.getBookRateList = data.Item;
+                    }
+                    else {
+                        alert(data.Message);
+                        location.reload();
+                    }
+                });
+            };
+
             $scope.getBookList = function () {
                 customService.postData('Book/getBookList', {}, function (data) {
                     $scope.count = data.Item.length;
@@ -30,5 +64,6 @@
             
 
             $scope.getBookList();
+            $scope.getBookRateList();
         });
     }}
